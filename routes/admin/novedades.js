36eadad0948d3b42ae3a7b4e2092c992f31d6c89
Elias Bulesevich <ebulesevich@frba.utn.edu.ar>
2023-08-05 17:07:23 -0300

    Tarea M6U2

---
+++ routes/admin/novedades.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  res.render('admin/novedades', {
+    layout: 'admin/layout',
+    persona: req.session.nombre,
+    novedades
+
+  });
+});
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar',{
+    layout: 'admin/layout'
+  })
+});
+
+router.post('/agregar', async (req, res, next) => {
+  try{
+    if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo!= ""){
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    }else{
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true, message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch(error){
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se cargo la novedad'
+    });
+  }
+});
+
+router.get('/eliminar/:id', async(req, res, next) => {
+  var id = req.params.id; // para capturar el id
+  await novedadesModel.deleteNovedadById(id);
+  res.redirect('/admin/novedades');
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+router.post('/modificar', async (req, res, next) => {
+  try{
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.modidicarNovedadById(obj, req.body.id);
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true, message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modificó la novedad'
+    })
+  }
+})  
+
+
+
+module.exports = router;
